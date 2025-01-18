b0433eecd54f0527e63ce70f71a6920ee24e99a4
cherrydub <cherrydub@gmail.com>
2025-01-18 21:12:06 +0000

    fix: add themeprovider props

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,12 @@ export default function RootLayout ({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
           {process.env.NODE_ENV === 'development' && <ViewportIndicator />}
         </ThemeProvider>
