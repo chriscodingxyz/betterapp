c394e38e7d4b5bee27bf98816b34c28fb75f22eb
cherrydub <cherrydub@gmail.com>
2025-02-09 13:47:01 +0000

    refactor: no pre-variable

--- auth/auth-client.ts
+++ auth/auth-client.ts
@@ -1,9 +1,7 @@
 import { createAuthClient } from 'better-auth/react'
 
-const baseURL = process.env.BETTER_AUTH_URL
-
 export const authClient = createAuthClient({
-  baseURL: baseURL
+  baseURL: process.env.BETTER_AUTH_URL
 })
 
 export const { signIn, signOut, useSession } = authClient
