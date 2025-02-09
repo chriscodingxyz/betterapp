a35530e6670216f44e6d85df7360fced3ac3facf
cherrydub <cherrydub@gmail.com>
2025-02-09 15:53:03 +0000

    chore: alter homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,28 @@
+// 'use client'
 import AuthSignInCheck from '@/components/dev/AuthSignInCheck'
 import SetupChecks from '@/components/dev/SetupChecks'
 // import { ReadmeContent } from '@/components/dev/ReadmeContent'
 import { ThemeToggle } from '@/components/theme/ThemeToggle'
+import Image from 'next/image'
+// import { useState } from 'react'
 
 export default function Home () {
+  // const [isExpanded, setIsExpanded] = useState(false)
+
   return (
     <div className='flex flex-col items-center justify-center min-h-screen gap-8 p-4'>
       <div className='flex flex-col items-center gap-4'>
+        <div className='flex flex-col items-center gap-0 cursor-pointer'>
+          <Image src='/mercury.png' alt='logo' width={100} height={100} />
+          <div className='flex flex-col items-center gap-4 border-primary'>
+            <SetupChecks />
+            {/* <AuthSignInCheck /> */}
+            {/* <ReadmeContent /> */}
+          </div>
+        </div>
+
         <ThemeToggle />
-        {/* <h1 className='text-md font-bold'>Setup Checks</h1> */}
       </div>
-      <SetupChecks />
-      <AuthSignInCheck />
-      {/* <ReadmeContent /> */}
     </div>
   )
 }
--- components/dev/AuthSignInCheck.tsx
+++ components/dev/AuthSignInCheck.tsx
@@ -18,7 +18,7 @@ export default function AuthSignInCheck () {
           })
         }}
       >
-        Sign Up
+        😎
       </Button>
     </div>
   )
